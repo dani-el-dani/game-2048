e6663daea003aee11cbb27575fb7dc8e3ea937dc
dani-el-dani <getdani11@gmail.com>
2024-12-12 12:24:38 -0500

    Add the game to the page (with dummy style)

--- Game_2048.js
+++ Game_2048.js
@@ -1,16 +1,16 @@
-let level = 4;
+let gameDifficulty = 4;
 const gameNumbers = [8,0,4,4,16,2,2,2,8,8,2,4,128,64,64,512];
 const bodyElement = document.querySelector("body")
 let bodyhtml = ``;
 gameNumbers.forEach((value,index) => {
     if(value === 0){
-        bodyhtml += `<button class = "number-square"> </button>`;
+        bodyhtml += `<div class = "number-square"></div>`;
     }
     else{
-        bodyhtml += `<button class = "number-square">${value}</button>`;
+        bodyhtml += `<div class = "number-square">${value}</div>`;
     }
     
-    if(index > 0 && index % level === level-1){
+    if(index > 0 && index % gameDifficulty === gameDifficulty-1){
         bodyhtml += `<br>`;
     }
 });
@@ -28,17 +28,18 @@ bodyElement.addEventListener("keydown", (event) => {
     }
     else if(event.key === "ArrowRight"){
         moveRight();
+        console.log(event.key);
     }
     
     bodyhtml = ``;
     gameNumbers.forEach((value,index) => {
         if(value === 0){
-            bodyhtml += `<button class = "number-square"> </button>`;
+            bodyhtml += `<div class = "number-square"> </div>`;
         }
         else{
-            bodyhtml += `<button class = "number-square">${value}</button>`;
+            bodyhtml += `<div class = "number-square">${value}</div>`;
         }
-        if(index > 0 && index % level === level-1){
+        if(index > 0 && index % gameDifficulty === gameDifficulty-1){
             bodyhtml += `<br>`;
         }
     });
@@ -47,53 +48,53 @@ bodyElement.addEventListener("keydown", (event) => {
 });
 
 function moveLeft(){
-    for(let i = 0; i < n; i++){
+    for(let i = 0; i < gameDifficulty; i++){
         const row = [];
-        for(let j = 0; j < level; j++){
-            row[j] = gameNumbers[i * level + j];
+        for(let j = 0; j < gameDifficulty; j++){
+            row[j] = gameNumbers[i * gameDifficulty + j];
         }
         addNumbersInRow(row);
-        for(let j = 0; j < level; j++){
-            gameNumbers[i * level + j] = row[j];
+        for(let j = 0; j < gameDifficulty; j++){
+            gameNumbers[i * gameDifficulty + j] = row[j];
         }
     }
 }
 
 function moveUp(){
-    for(let i = 0; i < level; i++){
+    for(let i = 0; i < gameDifficulty; i++){
         const row = [];
-        for(let j = 0; j < level; j++){
-            row[j] = gameNumbers[i + level * j];
+        for(let j = 0; j < gameDifficulty; j++){
+            row[j] = gameNumbers[i + gameDifficulty * j];
         }
         addNumbersInRow(row);
-        for(let j = 0; j < level; j++){
-            gameNumbers[i + level * j] = row[j];
+        for(let j = 0; j < gameDifficulty; j++){
+            gameNumbers[i + gameDifficulty * j] = row[j];
         }
     }
 }
 
 function moveRight(){
-    for(let i = 0; i < level; i++){
+    for(let i = 0; i < gameDifficulty; i++){
         const row = [];
-        for(let j = level-1; j >= 0 ; j--){
-            row[level - 1 -j] = gameNumbers[i * level + j];
+        for(let j = gameDifficulty-1; j >= 0 ; j--){
+            row[gameDifficulty - 1 -j] = gameNumbers[i * gameDifficulty + j];
         }
         addNumbersInRow(row);
-        for(let j = level-1; j >= 0 ; j--){
-            gameNumbers[i * level + j] = row[level - 1 - j];
+        for(let j = gameDifficulty-1; j >= 0 ; j--){
+            gameNumbers[i * gameDifficulty + j] = row[gameDifficulty - 1 - j];
         }
     }
 }
 
 function moveDown(){
-    for(let i = 0; i < n; i++){
+    for(let i = 0; i < gameDifficulty; i++){
         const row = [];
-        for(let j = level-1; j >= 0 ; j--){
-            row[level - 1 -j] = gameNumbers[i + level * j];
+        for(let j = gameDifficulty-1; j >= 0 ; j--){
+            row[gameDifficulty - 1 -j] = gameNumbers[i + gameDifficulty * j];
         }
         addNumbersInRow(row);
-        for(let j = level-1; j >= 0 ; j--){
-            gameNumbers[i + level * j] = row[level - 1 - j];
+        for(let j = gameDifficulty-1; j >= 0 ; j--){
+            gameNumbers[i + gameDifficulty * j] = row[gameDifficulty - 1 - j];
         }
     }
 }
