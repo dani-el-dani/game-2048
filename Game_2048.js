ce648b2897a7fc7f80a3901a98810cc993235d23
dani-el-dani <getdani11@gmail.com>
2025-02-18 05:43:42 -0500

    Adding animation and cleaning up a bit

--- Game_2048.js
+++ Game_2048.js
@@ -1,5 +1,6 @@
-let gameDifficulty;
+let gameWidth;
 let gameNumbers = [];
+let addedTiles;
 let undoGameNumbers;
 let previousScore;
 let isGameStarted = false;
@@ -19,23 +20,25 @@ const highScoreHtml = document.querySelector(".js-high-score");
 const restartButton = document.querySelectorAll(".js-restart-button");
 const undotButton = document.querySelector(".js-undo-button");
 const menuButton = document.querySelector(".js-menu-button");
-const gameDifficultyButtons = document.querySelectorAll(".js-difficulty-buttons");
+const gameWidthButtons = document.querySelectorAll(".js-gameWidth-buttons");
 const keepGoingButton = document.querySelector(".js-keep-going");
 
 
 menuButton.addEventListener(('click'), () => {
     isGameStarted = false;
+    addedTiles = [];
     gameNumbers = [];
     scoreHtml.innerHTML = 0;
     highScoreHtml.innerHTML = 0;
     document.querySelector(".js-game-menu").style.display = "block";
+    bodyElement.style.overscrollBehavior = "auto";
     
 })
-gameDifficultyButtons.forEach((button) => {
+gameWidthButtons.forEach((button) => {
     button.addEventListener(('click') , () => {
-        gameDifficulty = Number(button.dataset.gameDifficulty);
-        playAreaGrid.style.gridTemplateColumns = `repeat(${gameDifficulty}, 1fr)`;
-        playAreaGrid.style.fontSize = `${75/gameDifficulty}px`;
+        gameWidth = Number(button.dataset.gameWidth);
+        playAreaGrid.style.gridTemplateColumns = `repeat(${gameWidth}, 1fr)`;
+        playAreaGrid.style.fontSize = `${75/gameWidth}px`;
         reStartGame();
         document.querySelector(".js-game-menu").style.display = "none"
     })
@@ -45,13 +48,14 @@ function reStartGame(){
     isGameStarted = true;
     isGameWin = {win: false, keepGoing: false};
     levelHighScore = null;
-    savedHighScores.forEach((element) =>{
-        if(element.gameDifficulty === gameDifficulty){
-            levelHighScore = element;
-        }
+    addedTiles = [];
+    
+    levelHighScore = savedHighScores.find((element) => {
+        return element.gameWidth === gameWidth;
     });
+    
     if(!levelHighScore){
-        levelHighScore = {'gameDifficulty' : gameDifficulty, 'highScore': 0};
+        levelHighScore = {'gameWidth' : gameWidth, 'highScore': 0};
         savedHighScores.push(levelHighScore);
         localStorage.setItem("high scores", JSON.stringify(savedHighScores));
     }
@@ -59,15 +63,18 @@ function reStartGame(){
     isgameOver = false;
     score = 0;
     previousScore = score;
-    gameNumbers = Array(gameDifficulty * gameDifficulty).fill(0);
+    gameNumbers = Array(gameWidth * gameWidth).fill(0);
     
     addNewNumberToGame();
     undoGameNumbers = [...gameNumbers];
     updatePlayArea();
     scoreHtml.innerHTML = score;
     highScoreHtml.innerHTML = levelHighScore.highScore;
+    bodyElement.style.overscrollBehavior = "none";
     gameOverPopUp.style.display = "none";
 }
+
+// swipe event listner start.
 
 let touchStartX;
 let touchStartY;
@@ -83,155 +90,114 @@ bodyElement.addEventListener("touchend", (event) => {
     touchEndX = event.changedTouches[0].screenX;
     touchEndY = event.changedTouches[0].screenY;
     let key;
-
-    if(Math.abs(touchStartX - touchEndX) > Math.abs(touchStartY - touchEndY)){
-        if(touchStartX > touchEndX){
-            key = "ArrowLeft";
-        }
-        else{
-            key = "ArrowRight"
-        }
+    if(Math.abs(touchStartY - touchEndY) < 10 && Math.abs(touchStartX - touchEndX) < 10){
+        key = "none";
     }
     else{
-        if(touchStartY > touchEndY){
-            key = "ArrowUp";
+        if(Math.abs(touchStartX - touchEndX) > Math.abs(touchStartY - touchEndY)){
+            if(touchStartX > touchEndX){
+                key = "ArrowLeft";
+            }
+            else{
+                key = "ArrowRight"
+            }
         }
         else{
-            key = "ArrowDown"
+            if(touchStartY > touchEndY){
+                key = "ArrowUp";
+            }
+            else{
+                key = "ArrowDown"
+            }
         }
     }
+    
 
-    if(isGameStarted){
-        let previousGameNumbers;
-        let isGameChanged;
-        if(!isgameOver && !isGameWin.win ){
-            previousGameNumbers = [...gameNumbers];
-            previousScore = score;
-            isGameChanged = false;
-            if(key === "ArrowDown"){
-                moveDown();
-            }
-            else if(key === "ArrowUp"){
-                moveUp();
-            }
-            else if(key === "ArrowLeft"){
-                moveLeft();
-            }
-            else if(key === "ArrowRight"){
-                moveRight();
-            }
-
-            if(score > levelHighScore.highScore){
-                levelHighScore.highScore = score;
-                savedHighScores.forEach((element) =>{
-                    if(element.gameDifficulty === gameDifficulty){
-                        element.highScore = score;
-                    }
-                });
-                localStorage.setItem("high scores", JSON.stringify(savedHighScores));
-                highScoreHtml.innerHTML = levelHighScore.highScore;
-
-            }
-        
-            for(let i = 0; i < gameDifficulty * gameDifficulty; i++){
-                if(gameNumbers[i] !== previousGameNumbers[i]){
-                    isGameChanged = true;
-                    break;
-                }
-            }
-            
-            if(isGameChanged){
-                undoGameNumbers = [...previousGameNumbers];
-                if(!isGameWin.keepGoing){
-                    chaeckGameWin();
-                }
-                if(!isGameWin.win){
-                    addNewNumberToGame();
-                    checkIfGameOver();
-                    updatePlayArea();
-                    scoreHtml.innerHTML = score;
-                    if(isgameOver){
-                        gameOverPopUp.style.display = "block";
-                    }
-                }
-                else{
-                    updatePlayArea();
-                    scoreHtml.innerHTML = score;
-                    document.querySelector('.js-game-win').style.display = "block";
-                }                     
-            }
-        }
+    if(isGameStarted && key !== "none"){
+        handleMove(key);
     }
 
 
 })
 
+// swipe event listner end
+
+// key down event listner start
+
+function handleMove(key){
+    let previousGameNumbers;
+    let isGameChanged;
+    if(!isgameOver && !isGameWin.win ){
+        previousGameNumbers = [...gameNumbers];
+        previousScore = score;
+        isGameChanged = false;
+        if(key === "ArrowDown"){
+            moveDown();
+        }
+        else if(key === "ArrowUp"){
+            moveUp();
+        }
+        else if(key === "ArrowLeft"){
+            moveLeft();
+        }
+        else if(key === "ArrowRight"){
+            moveRight();
+        }
+
+        if(score > levelHighScore.highScore){
+            levelHighScore.highScore = score;
+            savedHighScores.forEach((element) =>{
+                if(element.gameWidth === gameWidth){
+                    element.highScore = score;
+                }
+            });
+            localStorage.setItem("high scores", JSON.stringify(savedHighScores));
+            highScoreHtml.innerHTML = levelHighScore.highScore;
+
+        }
+    
+        for(let i = 0; i < gameWidth * gameWidth; i++){
+            if(gameNumbers[i] !== previousGameNumbers[i]){
+                isGameChanged = true;
+                break;
+            }
+        }
+        
+        if(isGameChanged){
+            undoGameNumbers = [...previousGameNumbers];
+            if(!isGameWin.keepGoing){
+                chaeckGameWin();
+            }
+            if(!isGameWin.win){
+                addNewNumberToGame();
+                checkIfGameOver();
+                updatePlayArea();
+                scoreHtml.innerHTML = score;
+                if(isgameOver){
+                    setTimeout(() => {
+                        gameOverPopUp.style.display = "block";
+                    }, 250);  
+                }
+            }
+            else{
+                updatePlayArea();
+                scoreHtml.innerHTML = score;
+                document.querySelector('.js-game-win').style.display = "block";
+            }                     
+        }
+    }
+}
 
 bodyElement.addEventListener("keydown", (event) => {
     if(isGameStarted){
-        let previousGameNumbers;
-        let isGameChanged;
-        if(!isgameOver && !isGameWin.win ){
-            previousGameNumbers = [...gameNumbers];
-            previousScore = score;
-            isGameChanged = false;
-            if(event.key === "ArrowDown"){
-                moveDown();
-            }
-            else if(event.key === "ArrowUp"){
-                moveUp();
-            }
-            else if(event.key === "ArrowLeft"){
-                moveLeft();
-            }
-            else if(event.key === "ArrowRight"){
-                moveRight();
-            }
-
-            if(score > levelHighScore.highScore){
-                levelHighScore.highScore = score;
-                savedHighScores.forEach((element) =>{
-                    if(element.gameDifficulty === gameDifficulty){
-                        element.highScore = score;
-                    }
-                });
-                localStorage.setItem("high scores", JSON.stringify(savedHighScores));
-                highScoreHtml.innerHTML = levelHighScore.highScore;
-
-            }
-        
-            for(let i = 0; i < gameDifficulty * gameDifficulty; i++){
-                if(gameNumbers[i] !== previousGameNumbers[i]){
-                    isGameChanged = true;
-                    break;
-                }
-            }
-            
-            if(isGameChanged){
-                undoGameNumbers = [...previousGameNumbers];
-                if(!isGameWin.keepGoing){
-                    chaeckGameWin();
-                }
-                if(!isGameWin.win){
-                    addNewNumberToGame();
-                    checkIfGameOver();
-                    updatePlayArea();
-                    scoreHtml.innerHTML = score;
-                    if(isgameOver){
-                        gameOverPopUp.style.display = "block";
-                    }
-                }
-                else{
-                    updatePlayArea();
-                    scoreHtml.innerHTML = score;
-                    document.querySelector('.js-game-win').style.display = "block";
-                }                     
-            }
-        }
+        handleMove(event.key);
     }
         
 });
 
+// key down event listner end
+
+// handles reset, undo and keep going buttons
 restartButton.forEach((button) => {
     button.addEventListener('click', () =>{
         if(isGameStarted){
@@ -243,6 +209,7 @@ restartButton.forEach((button) => {
 })
 undotButton.addEventListener('click', ()=>{
     if(isGameStarted){
+        addedTiles = [];
         undoGameNumbers.forEach((value,index) =>{
             gameNumbers[index] = value;
         });
@@ -261,17 +228,21 @@ undotButton.addEventListener('click', ()=>{
 keepGoingButton.addEventListener('click', () =>{
     isGameWin.win = false;
     isGameWin.keepGoing = true;
+    addedTiles = [];
     addNewNumberToGame();
     checkIfGameOver();
     updatePlayArea();
     scoreHtml.innerHTML = score;
     if(isgameOver){
-        gameOverPopUp.style.display = "block";
+        setTimeout(() => {
+            gameOverPopUp.style.display = "block";
+        }, 250)
     }
     document.querySelector('.js-game-win').style.display = "none";
     
 })
 
+// update the playing array after a swipe or a button press
 function updatePlayArea(){
     bodyhtml = ``;
     gameNumbers.forEach((value,index) => {
@@ -279,10 +250,17 @@ function updatePlayArea(){
             bodyhtml += `<div class="number-square number-square-0"><p class="number-text"></p></div>`;
         }
         else if(value < 4096){
-            bodyhtml += `<div class = "number-square number-square-${value}"><p class = "number-text">${value}</p></div>`;
+            
+            if(!addedTiles.includes(index))
+                bodyhtml += `<div class = "number-square number-square-${value}"><p class = "number-text">${value}</p></div>`;
+            else
+                bodyhtml += `<div class = "number-square number-square-${value} animate-zoom-in"><p class = "number-text">${value}</p></div>`;
         }
         else{
-            bodyhtml += `<div class = "number-square number-square-4096"><p class = "number-text">${value}</p></div>`;
+            if(!addedTiles.includes(index))
+                bodyhtml += `<div class = "number-square number-square-4096"><p class = "number-text">${value}</p></div>`;
+            else
+                bodyhtml += `<div class = "number-square number-square-4096" animate-zoom-in><p class = "number-text">${value}</p></div>`;  
         }
         
     });
@@ -291,60 +269,86 @@ function updatePlayArea(){
 
 
 function moveLeft(){
-    for(let i = 0; i < gameDifficulty; i++){
+    addedTiles = [];
+    for(let i = 0; i < gameWidth; i++){
         const row = [];
-        for(let j = 0; j < gameDifficulty; j++){
-            row[j] = gameNumbers[i * gameDifficulty + j];
+        for(let j = 0; j < gameWidth; j++){
+            row[j] = gameNumbers[i * gameWidth + j];
         }
-        addNumbersInRow(row);
-        for(let j = 0; j < gameDifficulty; j++){
-            gameNumbers[i * gameDifficulty + j] = row[j];
+
+        let rowAddedtiles = addNumbersInRow(row);
+        for(let j = 0; j < rowAddedtiles.length; j++){
+            addedTiles.push(i * gameWidth + rowAddedtiles[j]);
+        } 
+        
+        for(let j = 0; j < gameWidth; j++){
+            gameNumbers[i * gameWidth + j] = row[j];
         }
     }
 }
 
 function moveUp(){
-    for(let i = 0; i < gameDifficulty; i++){
+    addedTiles = [];
+    for(let i = 0; i < gameWidth; i++){
         const row = [];
-        for(let j = 0; j < gameDifficulty; j++){
-            row[j] = gameNumbers[i + gameDifficulty * j];
+        for(let j = 0; j < gameWidth; j++){
+            row[j] = gameNumbers[i + gameWidth * j];
         }
-        addNumbersInRow(row);
-        for(let j = 0; j < gameDifficulty; j++){
-            gameNumbers[i + gameDifficulty * j] = row[j];
+        
+        let rowAddedtiles = addNumbersInRow(row);
+        for(let j = 0; j < rowAddedtiles.length; j++){
+            addedTiles.push(i  + rowAddedtiles[j] * gameWidth);
+        } 
+
+        for(let j = 0; j < gameWidth; j++){
+            gameNumbers[i + gameWidth * j] = row[j];
         }
     }
 }
 
 function moveRight(){
-    for(let i = 0; i < gameDifficulty; i++){
+    addedTiles = [];
+    for(let i = 0; i < gameWidth; i++){
         const row = [];
-        for(let j = gameDifficulty-1; j >= 0 ; j--){
-            row[gameDifficulty - 1 -j] = gameNumbers[i * gameDifficulty + j];
+        for(let j = gameWidth-1; j >= 0 ; j--){
+            row[gameWidth - 1 -j] = gameNumbers[i * gameWidth + j];
         }
-        addNumbersInRow(row);
-        for(let j = gameDifficulty-1; j >= 0 ; j--){
-            gameNumbers[i * gameDifficulty + j] = row[gameDifficulty - 1 - j];
+        
+        let rowAddedtiles = addNumbersInRow(row);
+        for(let j = 0; j < rowAddedtiles.length; j++){
+            addedTiles.push((i + 1) * gameWidth - 1 - rowAddedtiles[j]);
+        } 
+
+        for(let j = gameWidth-1; j >= 0 ; j--){
+            gameNumbers[i * gameWidth + j] = row[gameWidth - 1 - j];
         }
     }
 }
 
 function moveDown(){
-    for(let i = 0; i < gameDifficulty; i++){
+    addedTiles = [];
+    for(let i = 0; i < gameWidth; i++){
         const row = [];
-        for(let j = gameDifficulty-1; j >= 0 ; j--){
-            row[gameDifficulty - 1 -j] = gameNumbers[i + gameDifficulty * j];
+        for(let j = gameWidth-1; j >= 0 ; j--){
+            row[gameWidth - 1 -j] = gameNumbers[i + gameWidth * j];
         }
-        addNumbersInRow(row);
-        for(let j = gameDifficulty-1; j >= 0 ; j--){
-            gameNumbers[i + gameDifficulty * j] = row[gameDifficulty - 1 - j];
+        
+        let rowAddedtiles = addNumbersInRow(row);
+        for(let j = 0; j < rowAddedtiles.length; j++){
+            addedTiles.push(i + (gameWidth - 1 - rowAddedtiles[j]) * gameWidth);
+        } 
+
+        for(let j = gameWidth-1; j >= 0 ; j--){
+            gameNumbers[i + gameWidth * j] = row[gameWidth - 1 - j];
         }
     }
 }
 
+// handling single row
 function addNumbersInRow(row){
     let i = 0;
     let j = i + 1;
+    let addedIneicesInRow = []
     while(i < row.length - 1){
         // finding the next non-empty cell.
         while(row[j] === 0){
@@ -371,6 +375,7 @@ function addNumbersInRow(row){
             row[i] = row[i] + row[j];
             score += row[i]; 
             row[j] = 0;
+            addedIneicesInRow.push(i);
             i++;
         }
 
@@ -391,15 +396,16 @@ function addNumbersInRow(row){
         }
         
     }
+    return addedIneicesInRow;
 }
 
 function addNewNumberToGame(){
     const checked = [];
     while(true){
-        if(checked.length === gameDifficulty * gameDifficulty){
+        if(checked.length === gameWidth * gameWidth){
             break;
         }
-        let index = Math.floor(Math.random() * gameDifficulty * gameDifficulty);
+        let index = Math.floor(Math.random() * gameWidth * gameWidth);
         let isIndexChecked = false;
         checked.forEach((value) => {
             if(value === index){
@@ -415,13 +421,16 @@ function addNewNumberToGame(){
         }
         else{
             gameNumbers[index] = Math.random() < 0.95 ? 2 : 4;
+            addedTiles.push(index);
             break;
         }
     }
 }
+
 function checkIfGameOver(){
     const gameHolder = [...gameNumbers];
     const scoreHolder = score;
+    const addedTilesHolder = [...addedTiles];
     isgameOver = true;
     for(let i = 0; i < 4; i++){
         if(i === 0){
@@ -437,7 +446,7 @@ function checkIfGameOver(){
             moveLeft();
         }
         
-        for(let j = 0; j < gameDifficulty * gameDifficulty; j++){
+        for(let j = 0; j < gameWidth * gameWidth; j++){
             if(gameNumbers[j] !== gameHolder[j]){
                 isgameOver = false;
                 break;
@@ -451,7 +460,9 @@ function checkIfGameOver(){
         }
     }
     score = scoreHolder;
+    addedTiles = [...addedTilesHolder];
 }
+
 function chaeckGameWin(){
     for(let i = 0; i < gameNumbers.length; i++){
         if(gameNumbers[i] === 2048){
